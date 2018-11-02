99dd03195d124812421970387ccb08741c720050
JCarlosCS <cortes.cs91@gmail.com>
2018-11-02 14:32:22 -0600

    Filter...Needs to be changed into ViewModel logic.

--- app/src/main/java/com/jcdev/bestplaystv/view/activity/MainActivity.kt
+++ app/src/main/java/com/jcdev/bestplaystv/view/activity/MainActivity.kt
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.jcdev.bestplaystv.R
+import com.jcdev.bestplaystv.model.Game
 import kotlinx.coroutines.*
 import org.json.JSONObject
 
 class MainActivity : PlaysActivity() {
+
+    lateinit var popularVideoGames : ArrayList<Game>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,12 @@ class MainActivity : PlaysActivity() {
             if(gamesResponse.isSuccessful) {
                 val gamesEncoded = gamesResponse.body()
                 val debug = true
+
+
+                gamesEncoded?.content?.games?.asSequence()?.filter { it.value.stats.videos > 300 }?.forEach {
+                    popularVideoGames.add(it.value)
+                }
+
             } else {
                 val responseError = gamesResponse.errorBody()
                 val debugError = true;
