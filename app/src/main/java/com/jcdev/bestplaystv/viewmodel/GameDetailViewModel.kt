29d915ebf840e1580dc96955d1c8df73c125da55
iChepeman <cortes.cs91@gmail.com>
2019-09-18 17:00:46 -0500

    Updated code and moved the service calls to be background

--- app/src/main/java/com/jcdev/bestplaystv/viewmodel/GameDetailViewModel.kt
+++ app/src/main/java/com/jcdev/bestplaystv/viewmodel/GameDetailViewModel.kt
@@ -4,31 +4,41 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jcdev.bestplaystv.model.Game
 import com.jcdev.bestplaystv.model.Video
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class GameDetailViewModel(val game : Game) : PlaysViewModel() {
+class GameDetailViewModel(val game: Game) : PlaysViewModel() {
 
-    private val _randomGameVideos : MutableLiveData<List<Video>> = MutableLiveData()
-    val randomGameVideos : LiveData<List<Video>>
+    private val viewModelJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Default + viewModelJob)
+
+
+    private val _randomGameVideos: MutableLiveData<List<Video>> = MutableLiveData()
+    val randomGameVideos: LiveData<List<Video>>
         get() {
             return _randomGameVideos
         }
 
     fun getRandomGameVideos() {
-        GlobalScope.launch(Dispatchers.Main) {
+        serviceScope.launch {
 
             val videoRequest = playsTransport.getRandomVideosById(gameId = game.id)
             val videoResponse = videoRequest.await()
-            if(videoResponse.isSuccessful) {
+            if (videoResponse.isSuccessful) {
                 val videoEnconded = videoResponse.body()
 
-                _randomGameVideos.value = videoEnconded?.content?.items
+                _randomGameVideos.postValue(videoEnconded?.content?.items)
 
             } else {
                 val responseError = videoResponse.errorBody()
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
--- app/src/main/java/com/jcdev/bestplaystv/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/jcdev/bestplaystv/viewmodel/MainViewModel.kt
@@ -3,40 +3,46 @@ package com.jcdev.bestplaystv.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jcdev.bestplaystv.model.Game
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MainViewModel : PlaysViewModel() {
+    private val viewModelJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Default + viewModelJob)
 
-
-    private val _popularVideoGames : MutableLiveData<ArrayList<Game>> = MutableLiveData()
-    val popularVideoGames : LiveData<ArrayList<Game>>
-    get() {
-         return _popularVideoGames
-    }
+    private val _popularVideoGames: MutableLiveData<ArrayList<Game>> = MutableLiveData()
+    val popularVideoGames: LiveData<ArrayList<Game>>
+        get() {
+            return _popularVideoGames
+        }
 
 
     fun requestMostPopularGames() {
-        GlobalScope.launch(Dispatchers.Main) {
+        serviceScope.launch {
             val videoGamesList = ArrayList<Game>()
             val gamesRequest = playsTransport.getGames()
             val gamesResponse = gamesRequest.await()
-            if(gamesResponse.isSuccessful) {
+            if (gamesResponse.isSuccessful) {
                 val gamesEncoded = gamesResponse.body()
 
-                gamesEncoded?.content?.games?.asSequence()?.sortedByDescending{ it.value.stats.videos }?.
-                    take(20)?.
-                    forEach {
+                gamesEncoded?.content?.games?.asSequence()
+                    ?.sortedByDescending { it.value.stats.videos }?.take(20)?.forEach {
                         it.value.thumbnail.replace("exmedium", "exlarge")
                         videoGamesList.add(it.value)
-                }
+                    }
 
-                _popularVideoGames.value = videoGamesList
+                _popularVideoGames.postValue(videoGamesList)
 
             } else {
                 val responseError = gamesResponse.errorBody()
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
